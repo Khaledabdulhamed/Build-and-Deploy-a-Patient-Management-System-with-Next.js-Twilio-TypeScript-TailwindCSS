3874e78b985cc2d01f7914bc6de51175cdbedb3c
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2024-07-10 14:50:37 -0400

    second commit

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -4,29 +4,21 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
-import {  Form} from "@/components/ui/form"
+import {  Form, FormControl} from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import { useState } from "react"
 import SubmitButton from "../ui/SubmitButton"
 import { userFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
+import { FormFieldType } from "./PatientForm"
+import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group"
+import { GenderOptions } from "@/constants"
 
-export enum FormFieldType {
-    INPUT = 'input',
-    TEXTAREA = 'textarea',
-    PHONE_INPUT = 'phoneInput',
-    CHECKBOX = 'checkbox',
-    DATE_PICKER='datePicker',
-    SELECT='select',
-    SKELETON='skeleton',
-}
- 
 
- 
-const PatientForm=() => {
+const RegisterForm=({user}: {user: User}) => {
     const router = useRouter()
-const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof userFormValidation>>({
@@ -54,10 +46,15 @@ const [isLoading, setIsLoading] = useState(false)
   return (
 
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-            <h1 className="header">Hi there 👋</h1>
-            <p className="text-dark-700">Schedule your first appointment</p>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+        <section className="space-y-4">
+            <h1 className="header">Welcome 👋</h1>
+            <p className="text-dark-700">Let us know more about yourself</p>
+        </section>
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1"> 
+            <h2 className="sub-header">Personal Information</h2>
+          </div>
         </section>
 
         <CustomFormField
@@ -69,7 +66,8 @@ const [isLoading, setIsLoading] = useState(false)
          iconSrc= "/assets/icons/user.svg"
          iconAlt="user"    
          />
-          <CustomFormField
+        <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField
         fieldType = {FormFieldType.INPUT}
          control={form.control}
          name="email"
@@ -86,7 +84,40 @@ const [isLoading, setIsLoading] = useState(false)
          placeholder = "862-379-8312"
        
          />
+         </div>
 
+         <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField
+        fieldType = {FormFieldType.DATE_PICKER}
+         control={form.control}
+         name="birthDate"
+         label= "Date of Birth"
+            
+         />
+           <CustomFormField
+        fieldType = {FormFieldType.SKELETON}
+         control={form.control}
+         name="gender"
+         label= "Gender"
+         renderSkeleton={(field) => (
+          <FormControl>
+            <RadioGroup className="flex h-11 gap-6 xl:justify-between" onValueChange={field.onChange} defaultValue={field.value}>
+              {GenderOptions.map((option) => (
+                <div key={option} className="radio-group">
+                  <RadioGroupItem value={option} id={option} />
+                  <label htmlFor={option} className="cursor-pointer">
+                    {option}
+                  </label>
+                </div>
+              ))}
+            </RadioGroup>
+          </FormControl>
+         )}
+       
+         />
+         </div>
+
+     
       <SubmitButton isLoading={isLoading}>
         Get Started
       </SubmitButton>
@@ -95,4 +126,4 @@ const [isLoading, setIsLoading] = useState(false)
   )
 }
 
-export default PatientForm
+export default RegisterForm
